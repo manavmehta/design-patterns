d63c4d5f9a829e504bf2b371ec350903fdf46582
Manav Mehta <tmanavmehta@gmail.com>
2025-04-13 00:12:49 +0530

    bump to java 22, refactor

--- src/main/java/BookingManager.java
+++ src/main/java/BookingManager.java
@@ -2,6 +2,8 @@ import dependencies.Pair;
 import entities.*;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class BookingManager {
     Map<Integer, Cinema> cinemas;
@@ -24,130 +26,121 @@ public class BookingManager {
         return INSTANCE;
     }
 
-    public void addCinema(int cinemaId, int cityId, int screenCount, int screenRow, int screenColumn){
-        List<Screen> screensInCinema = new ArrayList<>();
-        for (var i=0; i<screenCount; i++){
-            var screen = new Screen(i, screenRow, screenColumn);
-            screensInCinema.add(screen);
-        }
-        var cinema = new Cinema(cinemaId, screensInCinema);
+    public void addCinema(int cinemaId, int cityId, int screenCount, int screenRow, int screenColumn) {
+        List<Screen> screens = IntStream.range(0, screenCount)
+                .mapToObj(i -> new Screen(i, screenRow, screenColumn))
+                .collect(Collectors.toList());
+
+        Cinema cinema = new Cinema(cinemaId, screens);
         cinemas.put(cinemaId, cinema);
 
-        var cinsByCity = cinemasByCity.getOrDefault(cityId, new ArrayList<>());
-        cinsByCity.add(cinemaId);
-        cinemasByCity.put(cityId, cinsByCity);
-        return;
+        cinemasByCity.computeIfAbsent(cityId, _ -> new ArrayList<>()).add(cinemaId);
     }
 
-    public void addShow(int showId, int movieId, int cinemaId, int screenId, long startTime, long endTime){
-        var cinema = cinemas.get(cinemaId);
-        var screen = cinema.getScreens().get(screenId);
-        shows.put(showId, new Show(showId, movieId, cinemaId, screen.rows, screen.columns, screenId, startTime, endTime));
-        List<Integer> shows = showsByCinemaId.getOrDefault(cinemaId, new ArrayList<>());
-        shows.add(showId);
-        showsByCinemaId.put(cinemaId, shows);
-        return;
+    public void addShow(int showId, int movieId, int cinemaId, int screenId, long startTime, long endTime) {
+        Screen screen = cinemas.get(cinemaId).getScreens().get(screenId);
+
+        Show show = new Show(showId, movieId, cinemaId, screen.getRows(), screen.getColumns(), screenId, startTime, endTime);
+        shows.put(showId, show);
+
+        showsByCinemaId.computeIfAbsent(cinemaId, _ -> new ArrayList<>()).add(showId);
     }
 
-    public List<Integer> listCinemas(int movieId, int cityId){
-        var cinemas = cinemasByCity.getOrDefault(cityId, new ArrayList<>());
-        Set<Integer> selectedCinemas = new HashSet<>();
-        for(var cinId: cinemas){
-            var allShows = showsByCinemaId.getOrDefault(cinId, new ArrayList<>());
-            for(var showId: allShows){
-                var show = shows.get(showId);
-                if (show.getMovieId()==movieId){
-                    selectedCinemas.add(cinId);
-                }
-            }
-        }
-        return selectedCinemas.stream().toList();
+    public List<Integer> listCinemas(int movieId, int cityId) {
+        return cinemasByCity.getOrDefault(cityId, List.of()).stream()
+                .filter(cinemaId -> showsByCinemaId.getOrDefault(cinemaId, List.of()).stream()
+                        .map(shows::get)
+                        .anyMatch(show -> show.getMovieId() == movieId))
+                .distinct()
+                .toList();
     }
 
-    public List<Integer> listShows(int movieId, int cinemaId){
-        Set<Integer> selectedShows = new HashSet<>();
-        var allShows = showsByCinemaId.getOrDefault(cinemaId, new ArrayList<>());
-        for(var showId: allShows){
-            var show = shows.get(showId);
-            if (show.getMovieId()==movieId){
-                selectedShows.add(showId);
-            }
-        }
-        return selectedShows.stream().toList();
+    public List<Integer> listShows(int movieId, int cinemaId) {
+        return showsByCinemaId.getOrDefault(cinemaId, List.of()).stream()
+                .filter(showId -> shows.get(showId).getMovieId() == movieId)
+                .distinct()
+                .toList();
     }
 
-    public int getFreeSeatsCount(int showId){
-        var show = shows.get(showId);
-        return show.getFreeSeats();
+    public int getFreeSeatsCount(int showId) {
+        return shows.get(showId).getFreeSeats();
     }
 
-    public ArrayList<Pair<Integer, Integer>> bookTicket(int ticketId, int showId, int ticketsCount){
-        var show = shows.get(showId);
-        var ticket = new Ticket(ticketId, showId, ticketsCount);
-        ArrayList<Pair<Integer, Integer>> bookedSeats = new ArrayList<>();
-        if (ticketsCount <= show.getFreeSeats()){
-            var seatsInLine = bookSeatsInLine(show, ticketsCount);
-            if (!seatsInLine.isEmpty()) {
-                bookedSeats = seatsInLine;
-            }
-            else {
-                bookedSeats = getDisconnectedSeats(show, ticketsCount);
-            }
-        }
+    public List<Pair<Integer, Integer>> bookTicket(int ticketId, int showId, int ticketsCount) {
+        Show show = shows.get(showId);
+        Ticket ticket = new Ticket(ticketId, showId, ticketsCount);
+
+        List<Pair<Integer, Integer>> bookedSeats = ticketsCount <= show.getFreeSeats()
+                ? bookSeatsInLine(show, ticketsCount)
+                : getDisconnectedSeats(show, ticketsCount);
+
         ticket.setSeats(bookedSeats);
         tickets.put(ticketId, ticket);
         show.setFreeSeats(show.getFreeSeats() - bookedSeats.size());
+
         return bookedSeats;
     }
 
-    public ArrayList<Pair<Integer, Integer>> bookSeatsInLine(Show show, int ticketsCount){
-        ArrayList<Pair<Integer, Integer>> bookedSeats = new ArrayList<>();
-        for (int i = 0; i < show.getRows(); i++){
+    public ArrayList<Pair<Integer, Integer>> bookSeatsInLine(Show show, int ticketsCount) {
+        boolean[][] seats = show.getSeats();
+        int rows = show.getRows();
+        int cols = show.getCols();
+
+        for (int i = 0; i < rows; i++) {
+            ArrayList<Pair<Integer, Integer>> temp = new ArrayList<>();
             int booked = 0;
-            int j=0;
-            while(j < show.getCols() && booked < ticketsCount){
-                if (!show.getSeats()[i][j]){
+
+            for (int j = 0; j < cols && booked < ticketsCount; j++) {
+                if (!seats[i][j]) {
+                    temp.add(new Pair<>(i, j));
                     booked++;
-                    bookedSeats.add(new Pair<>(i, j));
+                } else {
+                    temp.clear();
+                    booked = 0;
                 }
-                else{
-                    booked=0;
-                    bookedSeats.clear();
-                }
-                j++;
             }
-            if (booked == ticketsCount){
-                break;
+
+            if (booked == ticketsCount) {
+                return temp;
             }
         }
-        return bookedSeats;
+
+        return new ArrayList<>();
     }
 
-    public ArrayList<Pair<Integer, Integer>> getDisconnectedSeats(Show show, int ticketsCount){
-        ArrayList<Pair<Integer, Integer>> bookedSeats = new ArrayList<>();
-        for (int i = 0; i < show.getRows(); i++){
+    public ArrayList<Pair<Integer, Integer>> getDisconnectedSeats(Show show, int ticketsCount) {
+        boolean[][] seats = show.getSeats();
+        int rows = show.getRows();
+        int cols = show.getCols();
+
+        for (int i = 0; i < rows; i++) {
+            ArrayList<Pair<Integer, Integer>> temp = new ArrayList<>();
             int booked = 0;
-            int j=0;
-            while(j < show.getCols() && booked < ticketsCount){
-                if (show.getSeats()[i][j]){
+
+            for (int j = 0; j < cols && booked < ticketsCount; j++) {
+                if (seats[i][j]) {
+                    temp.add(new Pair<>(i, j));
                     booked++;
-                    bookedSeats.add(new Pair<>(i, j));
+                } else {
+                    temp.clear();
+                    booked = 0;
                 }
-                j++;
             }
-            if (booked == ticketsCount){
-                break;
+
+            if (booked == ticketsCount) {
+                return temp;
             }
         }
-        return bookedSeats;
+
+        return new ArrayList<>();
     }
 
-    public boolean cancelTicket(int ticketId){
-        if (!tickets.containsKey(ticketId) || tickets.get(ticketId).isCancelled()) return false;
-        var ticket = tickets.get(ticketId);
+    public boolean cancelTicket(int ticketId) {
+        Ticket ticket = tickets.get(ticketId);
+        if (ticket == null || ticket.isCancelled()) return false;
 
-        var show = shows.get(ticket.getShowId());
-        for (var seat: ticket.getSeats()){
+        Show show = shows.get(ticket.getShowId());
+        for (Pair<Integer, Integer> seat : ticket.getSeats()) {
             show.getSeats()[seat.first()][seat.second()] = false;
         }
 
